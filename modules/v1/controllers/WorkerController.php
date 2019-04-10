f323831c708f359d2e2d1b54d30ced52f231f226
Aekan <alpe15.1992@gmail.com>
2019-04-10 16:52:23 +0200

    @wip

--- modules/v1/controllers/WorkerController.php
+++ modules/v1/controllers/WorkerController.php
@@ -24,6 +24,34 @@ class WorkerController extends Controller {
         );
     }
 
+    public function behaviors() {
+        $behaviors = parent::behaviors();
+
+        $auth = $behaviors['authenticator'];
+
+        // remove authentication filter necessary because we need to
+        // add CORS filter and it should be added after the CORS
+        unset($behaviors['authenticator']);
+
+        // add CORS filter
+        $behaviors['corsFilter'] = [
+            'class' => '\yii\filters\Cors',
+            'cors' => [
+                'Origin' => ['*'],
+                'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+                'Access-Control-Request-Headers' => ['*'],
+            ],
+        ];
+
+        // re-add authentication filter
+        $behaviors['authenticator'] = $auth;
+
+        // avoid authentication on CORS-pre-flight requests (HTTP OPTIONS method)
+        $behaviors['authenticator']['except'] = ['options'];
+        return $behaviors;
+    }
+
+
     public function actionSum($a,$b) {
 
         return $a+$b;
