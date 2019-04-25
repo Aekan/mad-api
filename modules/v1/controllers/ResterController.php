d1b0975d2b328efe355c43625f5d0562ffd4d6f4
Aekan <alpe15.1992@gmail.com>
2019-04-25 10:13:33 +0200

    * extended Product action to get all Products
    * SF

--- modules/v1/controllers/ResterController.php
+++ modules/v1/controllers/ResterController.php
@@ -23,11 +23,12 @@ class ResterController extends Controller {
         return $a + $b;
     }
 
-
     public function actionProduct($id = false) {
         $myProduct = $id ? Product::getProdById($id) : Product::getAllProducts();
+        
         return $myProduct;
     }
+    
     public function actionYell(){
         return 'yellelek echoval de nem variablel';
     }
